fa9b955c88880c4379c72eaabd9f492fe128da81
Basel <baselsamy1999@gmail.com>
2025-11-01 16:54:01 +0100

    adding vercel analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/hooks/use-theme"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -22,6 +24,8 @@ export default function RootLayout({
         <ThemeProvider defaultTheme="light" storageKey="retro-ica-theme">
           {children}
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
