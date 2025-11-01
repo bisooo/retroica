122ca610e4847fa5669516ac4663ac3eca1cd49e
Basel <baselsamy1999@gmail.com>
2025-11-01 17:51:03 +0100

    product view scroll to top

--- src/app/product/layout.tsx
+++ src/app/product/layout.tsx
@@ -1,9 +1,15 @@
 import type React from "react"
+import ScrollToTop from "@/components/scroll-to-top"
 
 export default function ProductLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <div className="h-screen bg-white dark:bg-black overflow-hidden">{children}</div>
+  return (
+    <div className="h-screen bg-white dark:bg-black overflow-hidden">
+      <ScrollToTop />
+      {children}
+    </div>
+  )
 }
